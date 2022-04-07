5ad784f5be2eaa2f32265432f7d9a1f661da8d7f
Ed Cres <crespoed07@gmail.com>
2022-04-07 16:42:09 -0400

    Set up the database in the viewModel.

--- app/src/main/java/com/example/crrentals/SharedViewModel.kt
+++ app/src/main/java/com/example/crrentals/SharedViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.crrentals
 
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.crrentals.data.RentedItem
 import com.example.crrentals.data.Repository
 import com.example.crrentals.data.room.RentsRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 class SharedViewModel : ViewModel() {
 
@@ -10,5 +18,24 @@ class SharedViewModel : ViewModel() {
     private lateinit var roomDb: RentsRoomDatabase
     private lateinit var repo: Repository
 
+    private val _rentedItem = MutableLiveData<MutableList<RentedItem>>()
+    val rentedItem: LiveData<MutableList<RentedItem>> get() = _rentedItem
 
+    // SETUP //
+    fun startApplication(application: Application) {
+        roomDb = RentsRoomDatabase.getInstance(application)
+        repo = Repository(roomDb)
+        collectAllRentItems()
+    }
+    // SETUP //
+
+    // DATABASE QUERIES //
+    private fun collectAllRentItems() {
+        CoroutineScope(Dispatchers.IO).launch {
+            repo.allRentedItems.collect {
+                _rentedItem.postValue(it.toMutableList())
+            }
+        }
+    }
+    // DATABASE QUERIES //
 }
